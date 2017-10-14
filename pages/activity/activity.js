313c40259595522dd0f006005d6255449ca5edbb
chndgh <chndgh@163.com>
2017-10-14 16:27:03 +0800

    a

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -1,3 +1,5 @@
+// pages/selfInfo/selfInfo.js
+var util = require('../../utils/util.js');
 
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 const app = getApp();
@@ -9,12 +11,24 @@ Page({
     sliderLeft: 0,
     activityList:[],
     userInfo:{},
-    inputActivityTitle: '',
+    title: '',
+    year: '',
+    month: '',
+    day: '',
+    actStTime: '',
     startDate: '',
     startTime: '',
     endDate: '',
     endTime: '',
-    description: ''
+    address: '',
+    cost: '',
+    peoplenumner: '',
+    activitytypeIndex: '',
+    array: ['体育活动', '聚餐', '休闲娱乐', '旅游', '学习', '会议'],
+    publicity: '',
+    description: '',
+    activityStatus: {'1001':'未发布', '1002':'投票中', '1003':'待开始', '1004':'进行中', '1005':'已结束'},
+    activityItem: {}
   },
   getAvaliableActivities:function(){
     var that = this;
@@ -57,6 +71,13 @@ Page({
         });
       }
     });
+    // 通过setData更改Page()里面的data，动态更新页面的数据  
+    var date = new Date();
+    this.year = date.getFullYear();
+    this.month = date.getMonth() + 2;
+    this.day = date.getDate();
+    this.actStTime = this.year + this.month + this.day
+     
   },
   tabClick: function (e) {
     this.setData({
@@ -88,14 +109,24 @@ Page({
       scrollTop: this.data.scrollTop + 10
     })
   },
-  bindTitleChange: function (e) {
+
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      inputActivityTitle: e.detail.value
+      activitytypeIndex: e.detail.value
     })
+    console.log(e.detail.value)
+  },
+  bindThemeInput: function (e) {
+    this.setData({
+      title: e.detail.value
+    })
+    console.log(e.detail.value)
   },
   bindStartDateChange: function (e) {
     this.setData({
-      startDate: e.detail.value
+      startDate: e.detail.value,
+      actStTime: e.detail.value
     })
   },
   bindStartTimeChange: function (e) {
@@ -113,10 +144,35 @@ Page({
       endTime: e.detail.value
     })
   },
-  bindDescChange: function (e) {
+  bindActivityAddressInput: function (e) {
+    this.setData({
+      address: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindCostChange: function (e) {
+    this.setData({
+      cost: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindPeopleNumberChange: function (e) {
+    this.setData({
+      peoplenumber: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindActivityPublicity: function (e) {
+    this.setData({
+      Publicity: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindActivityDescInput: function (e) {
     this.setData({
       description: e.detail.value
     })
+    console.log(e.detail.value)
   },
   formSubmit: function (e) {
     var that = this;
@@ -124,11 +180,48 @@ Page({
     console.log(e.detail.value.startDate + " " + e.detail.value.startTime);
     var startTime = e.detail.value.startDate + " " + e.detail.value.startTime;
     var endTime = e.detail.value.endDate + " " + e.detail.value.endTime;
-    this.activityItem = { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, description: e.detail.value.description }
+
+    /* tile, time, address must not empty */
+    if (e.detail.value.title == "" ||
+      e.detail.value.address == "" ||
+      e.detail.value.startDate == "" ||
+      e.detail.value.startTime == "" ||
+      e.detail.value.endDate == "" ||
+      e.detail.value.endTime == "") {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '请填写完整信息',
+        success: function (res) {
+          if (res.confirm) {
+
+          }
+        }
+      })
+      return;
+    }
+    /* End time must bigger than start time */
+    if (endTime < startTime) {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '活动结束时间需要晚于开始时间',
+        success: function (res) {
+          if (res.confirm) {
+
+          }
+        }
+      })
+      return;
+    }
+
+
+    this.activityItem = { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, address: e.detail.value.address, cost: e.detail.value.cost, people: e.detail.value.peoplenumber, activitytype: that.data.array[e.detail.value.activitytype], publicity: e.detail.value.publicity, description: e.detail.value.description }
     console.log(this.activityItem);
+
     wx.request({
       url: "http://localhost:8080/activity/create",
-      data: { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, description: e.detail.value.description },
+      data: { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, address: e.detail.value.address, cost: e.detail.value.cost, people: e.detail.value.peoplenumber, activitytype: that.data.array[e.detail.value.activitytype], publicity: e.detail.value.publicity, description: e.detail.value.description },
       method: "POST",
       header: {
         "Content-Type": "application/json",
@@ -141,6 +234,16 @@ Page({
         console.log(err)
       }
     })
+
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+  successdeliver: function () {
+    wx.navigateTo({
+      url: "../success_deliver/success_deliver"
+    })
   }
+
 });
 
--- pages/createActivity/createActivity.js
+++ pages/createActivity/createActivity.js
@@ -6,29 +6,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputActivityTitle:'',
+    title:'',
+    year:'',
+    month:'',
+    day:'',
+    actStTime:'',
     startDate:'',
     startTime:'',
     endDate:'',
     endTime:'',
-    description:'',
-    activityItem:{},
-    array:['体育活动','聚餐','休闲娱乐','旅游','学习','会议']
+    address:'',
+    cost:'',
+    peoplenumner:'',
+    activitytypeIndex:'',
+    array:['体育活动','聚餐','休闲娱乐','旅游','学习','会议'],
+    publicity: '',
+    description: '',
+    activityItem: {}
   },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      index: e.detail.value
+      activitytypeIndex: e.detail.value
     })
+    console.log(e.detail.value)
   },
   bindThemeInput:function(e){
     this.setData({
-      inputActivityTitle: e.detail.value
+      title: e.detail.value
     })
+    console.log(e.detail.value)
   },
   bindStartDateChange: function (e) {
     this.setData({
-      startDate: e.detail.value
+      startDate: e.detail.value,
+      actStTime: e.detail.value
     })
   },
   bindStartTimeChange: function (e) {
@@ -48,13 +60,33 @@ Page({
   },
   bindActivityAddressInput: function (e) {
     this.setData({
-      Address: e.detail.value
+      address: e.detail.value
     })
+    console.log(e.detail.value)
+  },
+  bindCostChange: function (e) {
+    this.setData({
+      cost: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindPeopleNumberChange: function (e) {
+    this.setData({
+      peoplenumber: e.detail.value
+    })
+    console.log(e.detail.value)
+  },
+  bindActivityPublicity: function (e) {
+    this.setData({
+      Publicity: e.detail.value
+    })
+    console.log(e.detail.value)
   },
   bindActivityDescInput: function (e) {
     this.setData({
       description: e.detail.value
     })
+    console.log(e.detail.value)
   },
   formSubmit: function (e) {
     var that = this;
@@ -62,11 +94,48 @@ Page({
     console.log(e.detail.value.startDate + " " + e.detail.value.startTime);
     var startTime = e.detail.value.startDate + " " + e.detail.value.startTime;
     var endTime = e.detail.value.endDate + " " + e.detail.value.endTime;
-    this.activityItem = { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, description: e.detail.value.description }
+
+    /* tile, time, address must not empty */
+    if (e.detail.value.title == "" ||
+                  e.detail.value.address == "" ||
+                  e.detail.value.startDate == "" ||
+                  e.detail.value.startTime == "" ||
+                  e.detail.value.endDate == "" ||
+                  e.detail.value.endTime == "") {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '请填写完整信息',
+        success: function (res) {
+          if (res.confirm) {
+
+          }
+        }
+      })
+      return;
+    }
+    /* End time must bigger than start time */
+    if (endTime < startTime) {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '活动结束时间需要晚于开始时间',
+        success: function (res) {
+          if (res.confirm) {
+
+          }
+        }
+      })
+      return;
+    }
+
+
+    this.activityItem = { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, address: e.detail.value.address, cost: e.detail.value.cost, people: e.detail.value.peoplenumber, activitytype: that.data.array[e.detail.value.activitytype], publicity:e.detail.value.publicity, description: e.detail.value.description }
     console.log(this.activityItem);
+
     wx.request({
       url: "http://localhost:8080/activity/create",
-      data: { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, description: e.detail.value.description },
+      data: { id: 1, title: e.detail.value.title, startTime: startTime, endTime: endTime, address: e.detail.value.address, cost: e.detail.value.cost, people: e.detail.value.peoplenumber, activitytype: that.data.array[e.detail.value.activitytype], publicity: e.detail.value.publicity, description: e.detail.value.description },
       method: "POST",
       header: {
         "Content-Type": "application/json",
@@ -79,6 +148,7 @@ Page({
         console.log(err)
       }
     })
+
   },
   formReset: function () {
     console.log('form发生了reset事件')
@@ -92,7 +162,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+ 
+    // 通过setData更改Page()里面的data，动态更新页面的数据  
+    this.setData({
+      year: date.getFullYear(),
+      month : date.getMonth() + 1,
+      day : date.getDate(),
+      actStTime:year+month+day
+    });  
   },
 
   /**
@@ -142,5 +219,5 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
 })
